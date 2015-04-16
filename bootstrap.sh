753c26e383291feda4922653cee7d76e4f137ecd
Olivier <ofontannaud@cpmultimedia.com>
2015-04-16 11:58:41 +0200

    Remove centos ref from bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,6 @@
 # Add bootstrap modules
 mkdir -p /root/bootstrap/modules
 puppet module install --modulepath=/root/bootstrap/modules zack/r10k --version 2.5.4
-puppet module install --modulepath=/root/bootstrap/modules stahnma/epel --version 1.0.2
 puppet module install --modulepath=/root/bootstrap/modules stephenrjohnson/puppet --version 1.3.1
 puppet module install --modulepath=/root/bootstrap/modules hunner/hiera --version 1.1.1
 
@@ -18,7 +17,6 @@ r10k deploy environment -p
 if [ $? -eq 0 ]
 then
   puppet agent -t
-  chkconfig puppet on
   service puppet start
 else
   echo "Some part of the bootstrap process failed. Investigate the errors and proceed with manual bootstrapping."
