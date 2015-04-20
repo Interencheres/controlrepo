269815b37aa418c1a8b10e91ccd74de275f78d98
Olivier <ofontannaud@cpmultimedia.com>
2015-04-20 15:48:38 +0200

    Do not use stored configs

--- master.pp
+++ master.pp
@@ -2,6 +2,7 @@ Package {
   allow_virtual => true,
 }
 
+# Do not store config for bootstrapping, puppet need to deploy a PuppetDB server first
 class { '::puppet::master':
   storeconfigs => false,
   environments => directory,
